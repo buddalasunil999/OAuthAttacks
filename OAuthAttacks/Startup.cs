3dcf85a839ab42f7958d1674f89c76763ad0a568
buddalasunil999 <buddalasunil999@gmail.com>
2016-05-12 18:29:39 +1200

    initial commit

---
+++ OAuthAttacks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAuthAttacks.Startup))]
+namespace OAuthAttacks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
